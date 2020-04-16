9326e27709f8a2b3a41be1267c7c41807a9cdc6d
TheAnh <theanhptit94@gmail.com>
2020-04-16 09:02:05 +0700

    16/4

--- src/App.js
+++ src/App.js
@@ -3,43 +3,78 @@ import "./App.css";
 import TodoItem from "./componenets/TodoItem";
 import TrafficLight from "./componenets/TrafficLight";
 
-const RED = 0;
-const ORANGE = 1;
-const GREEN = 2;
+// const RED = 0;
+// const ORANGE = 1;
+// const GREEN = 2;
 class App extends Component {
   constructor() {
     super();
-    const todoItems = ["Work", "Play", "Sleep"];
     this.state = {
-      currentColor: GREEN,
+      todoList: [
+        { title: "Mua bim bim", isComplete: true },
+        { title: "Di da bong", isComplete: false },
+        { title: "Di do xang", isComplete: true },
+      ],
     };
-    setInterval(() => {
-      // console.log(this.state.currentColor);
-      this.setState({
-        currentColor: this.getNextColor(this.state.currentColor),
-      });
-    }, 1000);
+
+    // this.state = {
+    //   currentColor: GREEN,
+    // };
+    // setInterval(() => {
+    //   // console.log(this.state.currentColor);
+    //   this.setState({
+    //     currentColor: this.getNextColor(this.state.currentColor),
+    //   });
+    // }, 1000);
+
+    this.onItemClick = this.onItemClick.bind(this);
   }
 
-  getNextColor(color) {
-    switch (color) {
-      case RED:
-        return ORANGE;
-      case ORANGE:
-        return GREEN;
-      default:
-        return RED;
-    }
+  onItemClick(item) {
+    return () => {
+      // let cloneState = JSON.parse(JSON.stringify(this.state));
+      // console.log(cloneState);
+      // cloneState.todoList[index].isComplete = !cloneState.todoList[index]
+      //   .isComplete;
+      // this.setState(cloneState);
+      const isComplete = item.isComplete;
+      const { todoList } = this.state;
+      const index = todoList.indexOf(item);
+      this.setState({
+        todoList: [
+          ...todoList.slice(0,index),
+          {
+            ...item,
+            isComplete: !isComplete
+          },
+          ...todoList.slice(index+1)
+        ]
+      })
+    };
   }
+  // getNextColor(color) {
+  //   switch (color) {
+  //     case RED:s
+  //       return ORANGE;
+  //     case ORANGE:
+  //       return GREEN;
+  //     default:
+  //       return RED;
+  //   }
+  // }
   render() {
     return (
       <div className="App">
-        {this.todoItems.length > 0 &&
-          this.todoItems.map((item, index) => (
-            <TodoItem key={index} item={item} />
+        {this.state.todoList.length > 0 &&
+          this.state.todoList.map((item, index) => (
+            <TodoItem
+              key={index}
+              item={item}
+              onClick={this.onItemClick(item)}
+            />
           ))}
-        {this.todoItems.length === 0 && "Nothing here"}
-        <TrafficLight currentColor={this.state.currentColor} />}
+        {this.state.todoList.length === 0 && "Nothing here"}
+        {/* <TrafficLight currentColor={this.state.currentColor} /> */}
       </div>
     );
   }
--- src/componenets/TodoItem.js
+++ src/componenets/TodoItem.js
@@ -1,18 +1,34 @@
 import React, { Component } from "react";
 import "./TodoItem.css";
-var classNames = require("classnames");
+import classNames from "classnames";
 
 class TodoItem extends Component {
+  constructor(props) {
+    super(props);
+    this.onItemClick = this.onItemClick.bind(this);
+  }
+  onItemClick() {
+    this.props.item.isComplete = !this.props.item.isComplete;
+  }
   render() {
-    var className = classNames({
-      TodoItem: true,
-      "TodoItem-complete": this.props.item.isComplete,
-      // 'btn-over': !this.state.isPressed && this.state.isHovered
-    });
+    // var className = classNames({
+    //   TodoItem: true,
+    //   "TodoItem-complete": this.props.item.isComplete,
+    //   // 'btn-over': !this.state.isPressed && this.state.isHovered
+    // });
     // const { item } = this.props;
     // let className = "TodoItem";
-
-    return <div className={className}>{this.props.item.title}</div>;
+    const { item, onClick } = this.props;
+    return (
+      <div
+        onClick={onClick}
+        className={classNames("TodoItem", {
+          "TodoItem-complete": item.isComplete,
+        })}
+      >
+        {this.props.item.title}
+      </div>
+    );
   }
 }
 
