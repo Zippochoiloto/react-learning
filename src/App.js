d061c5f701a3f8acc65cb3e16bfd4d8940de2b99
TheAnh <theanhptit94@gmail.com>
2020-04-18 09:50:42 +0700

    18/4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 import TodoItem from "./componenets/TodoItem";
-import TrafficLight from "./componenets/TrafficLight";
+// import TrafficLight from "./componenets/TrafficLight";
+import checkAll from "./img/check.svg";
 
 // const RED = 0;
 // const ORANGE = 1;
@@ -10,6 +11,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      newItem: "",
+      currentState: "all",
       todoList: [
         { title: "Mua bim bim", isComplete: true },
         { title: "Di da bong", isComplete: false },
@@ -28,29 +31,74 @@ class App extends Component {
     // }, 1000);
 
     this.onItemClick = this.onItemClick.bind(this);
+    this.returnAll = this.returnAll.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.filter = this.filter.bind(this);
+    this.onKeyUp = this.onKeyUp.bind(this);
   }
 
   onItemClick(item) {
     return () => {
-      // let cloneState = JSON.parse(JSON.stringify(this.state));
-      // console.log(cloneState);
-      // cloneState.todoList[index].isComplete = !cloneState.todoList[index]
-      //   .isComplete;
-      // this.setState(cloneState);
       const isComplete = item.isComplete;
       const { todoList } = this.state;
       const index = todoList.indexOf(item);
       this.setState({
+        newItem: "",
+        currentState: "all",
         todoList: [
-          ...todoList.slice(0,index),
+          ...todoList.slice(0, index),
           {
             ...item,
-            isComplete: !isComplete
+            isComplete: !isComplete,
           },
-          ...todoList.slice(index+1)
-        ]
-      })
+          ...todoList.slice(index + 1),
+        ],
+      });
     };
+  }
+
+  onKeyUp(event) {
+    if (event.keyCode === 13) {
+      let text = event.target.value;
+      if (!text) {
+        return;
+      }
+      text = text.trim();
+      if (!text) {
+        return;
+      }
+
+      this.setState({
+        newItem: "",
+        currentState: "all",
+        todoList: [{ title: text, isComplete: false }, ...this.state.todoList],
+      });
+    }
+  }
+
+  onChange(event) {
+    console.log(event.target.value);
+    this.setState({
+      newItem: event.target.value,
+      todoList: [...this.state.todoList],
+    });
+    console.log(this.state);
+  }
+
+  filter() {
+    this.setState({
+      newItem: "",
+      currentState: "complete",
+      todoList: [...this.state.todoList],
+    });
+  }
+
+  returnAll() {
+    this.setState({
+      newItem: "",
+      currentState: "all",
+      todoList: [...this.state.todoList],
+    });
   }
   // getNextColor(color) {
   //   switch (color) {
@@ -63,17 +111,31 @@ class App extends Component {
   //   }
   // }
   render() {
+    let { newItem, currentState, todoList } = this.state;
+    if (currentState === "complete") {
+      todoList = todoList.filter((val) => val.isComplete === true);
+      console.log(todoList);
+    }
     return (
       <div className="App">
-        {this.state.todoList.length > 0 &&
-          this.state.todoList.map((item, index) => (
-            <TodoItem
-              key={index}
-              item={item}
-              onClick={this.onItemClick(item)}
-            />
+        <div className="Header">
+          <img src={checkAll} width={32} alt="" onClick={this.filter} />
+          <input
+            type="text"
+            placeholder="Add new Item"
+            value={newItem}
+            onChange={this.onChange}
+            onKeyUp={this.onKeyUp}
+          />
+        </div>
+        {todoList.length > 0 &&
+          todoList.map((item, index) => (
+            <TodoItem key={index} item={item} onClick={this.onItemClick(item)}>
+              what the hell
+            </TodoItem>
           ))}
         {this.state.todoList.length === 0 && "Nothing here"}
+        <button onClick={this.returnAll}>Return All</button>
         {/* <TrafficLight currentColor={this.state.currentColor} /> */}
       </div>
     );
--- src/componenets/TodoItem.js
+++ src/componenets/TodoItem.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import "./TodoItem.css";
 import classNames from "classnames";
+import checkImg from "../img/check.svg";
+import checkCompleteImg from "../img/check-complete.svg";
 
 class TodoItem extends Component {
   constructor(props) {
     super(props);
     this.onItemClick = this.onItemClick.bind(this);
+    this.state = {
+      isShowing: true,
+    };
   }
   onItemClick() {
     this.props.item.isComplete = !this.props.item.isComplete;
@@ -19,14 +24,22 @@ class TodoItem extends Component {
     // const { item } = this.props;
     // let className = "TodoItem";
     const { item, onClick } = this.props;
+    let url = checkImg;
+    console.log(this.state.isShowing);
+    console.log(this.props.children);
+    if (item.isComplete) {
+      url = checkCompleteImg;
+    }
     return (
       <div
-        onClick={onClick}
         className={classNames("TodoItem", {
           "TodoItem-complete": item.isComplete,
         })}
       >
+        <img onClick={onClick} width={32} alt="" src={url} />
         {this.props.item.title}
+
+        <div className="Content">{this.props.children}</div>
       </div>
     );
   }
